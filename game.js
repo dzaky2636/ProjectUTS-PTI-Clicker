a3007d67cfa6e6ca5148aaa9b2f9d69595fb6cd9
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-03-24 00:16:17 +0700

    Tambah clicker basic dan animasi klik

---
+++ game.js
@@ -0,0 +1,12 @@
+money = 0;
+moneyup = 1;
+
+function clicked(){
+    var moneyAnimation = document.createElement("p");
+    moneyAnimation.innerHTML = "+1";
+
+    money += moneyup;
+    document.getElementById("moneyDisplay").innerHTML = "Money: " + money;
+    document.getElementById("moneyAnimation").appendChild(moneyAnimation);
+    moneyAnimation.classList.add("moneyAnimation");
+}
